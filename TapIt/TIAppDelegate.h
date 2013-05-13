178d346d00b06a80702d88abc0091ed64c77ffb9
Hyung-Suk Kim <mastak@gmail.com>
2013-05-13 12:34:50 -0700

    Initial Commit

---
+++ TapIt/TIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TIAppDelegate.h
+//  TapIt
+//
+//  Created by Hyung-Suk Kim on 5/13/13.
+//  Copyright (c) 2013 Hyung-Suk Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TapIt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TapIt
+//
+//  Created by Hyung-Suk Kim on 5/13/13.
+//  Copyright (c) 2013 Hyung-Suk Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TIAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TIAppDelegate class]));
+    }
+}
